4939e040952bf1e0e8e60a34520618c6048f301d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-24 16:09:18 +0530

    Changed the title icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Category from './components/Category/Category';
+import SingleProduct from './components/SingleProduct/SingleProduct';
+import Newsletter from './components/SingleProduct/SingleProduct';
+
 function App() {
-    return <div>App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/category/:id" element={<Category/>}/>
+                <Route path="/product/:id" element={<SingleProduct/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
