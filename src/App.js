bfba046e798274eaed9753e24499940cec6b28a2
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-30 15:28:16 +0530

    Header section done

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import Home from './components/Home/Home';
 import Category from './components/Category/Category';
 import SingleProduct from './components/SingleProduct/SingleProduct';
 import Newsletter from './components/SingleProduct/SingleProduct';
+import AppContext from './utils/context'
 
 function App() {
     return (
         <BrowserRouter>
+        <AppContext>
+        <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/category/:id" element={<Category/>}/>
                 <Route path="/product/:id" element={<SingleProduct/>}/>
             </Routes>
+        <Newsletter/>
+        <Footer/>
+        </AppContext>    
         </BrowserRouter>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,53 @@
+import { useEffect, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import {TbSearch} from "react-icons/tb";
+import {CgShoppingCart} from "react-icons/cg";
+import {AiOutlineHeart} from "react-icons/ai";
+
+import Search from "./Search/Search";
+import Cart from "../Cart/Cart";
+import { Context } from "../../utils/context";
+
 import "./Header.scss";
+
 const Header = () => {
-    return <div>Header</div>;
+
+    const [scrolled, setScrolled] = useState(false);
+
+    const handleScroll = () =>{
+        const offset = window.scrollY
+        if(offset>200){
+            setScrolled(true)
+        }
+        else{
+            setScrolled(false)
+        }
+
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll",handleScroll)
+    },[]);
+
+    return <header className={`main-header ${scrolled ? "sticky-header":""} `}>
+        <div className="header-content">
+            <ul className="left">
+                <li>Home</li>
+                <li>About</li>
+                <li>Categories</li>
+            </ul>
+            <div className="center">ECOMIFY</div>
+            <div className="right">
+                <TbSearch/>
+                <AiOutlineHeart/>
+                <span className="cart-icon">
+                    <CgShoppingCart/>
+                    <span>5</span>
+                </span>
+            </div>
+        </div>
+    </header>;
 };
 
 export default Header;
