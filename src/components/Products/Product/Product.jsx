5167957e85244e50c59f8f43708f08e09d0aa548
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-31 10:32:54 +0530

    Home page products cards done

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,16 @@
 import "./Product.scss";
+import prod from "../../../assets/products/earbuds-prod-1.webp"
+
 const Product = () => {
-    return <div>Product</div>;
+    return <div className="product-card">
+        <div className="thumbnail">
+            <img src={prod} alt="" />
+        </div>
+        <div className="prod-details">
+            <span className="name">Product name</span>
+            <span className="price">&#8360; 499</span>
+        </div>
+    </div>;
 };
 
 export default Product;
