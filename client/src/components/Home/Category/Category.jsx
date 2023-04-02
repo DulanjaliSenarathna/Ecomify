15d34d0798459060d3866ef33feb5abc6e4c7399
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-02 11:18:58 +0530

    Fetched category items when click each categories type

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,12 +1,13 @@
+import {useNavigate} from "react-router-dom"
+
 import "./Category.scss";
-import cat1 from "../../../assets/category/cat-1.jpg";
-import Home from "../Home";
 
 const Category = ({categories}) => {
+    const navigate = useNavigate();
     return <div className="shop-by-category">
         <div className="categories">
             {categories?.data?.map((item) => ( // ? = Elvis Operator (optional chaining)-used while accessing some property on an object to avoid null or undefined errors
-                <div key={item.id} className="category">
+                <div key={item.id} className="category" onClick={()=> navigate(`/category/${item.id}`)}>
                     <img src={process.env.REACT_APP_DEV_URL + item.attributes.img.data.attributes.url} alt="Category1" />
                 </div>
             )
