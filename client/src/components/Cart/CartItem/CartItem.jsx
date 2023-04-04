5fb695744a4e3685cc741cf58640dfb87a12c84e
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-04 16:28:42 +0530

    Fetched cart item image

--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -11,7 +11,7 @@ const CartItem = () => {
         {cartItems.map(item =>(
             <div key={item.id} className="cart-product">
             <div className="img-container">
-                <img src={prod} alt="Product" />
+                <img src={process.env.REACT_APP_DEV_URL+ item.attributes.img.data[0].attributes.url} alt="Product" />
             </div>
             <div className="prod-details">
                 <span className="name">{item.attributes.title}</span>
