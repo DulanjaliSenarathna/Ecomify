5ad37dc8df8ddf47bd4278541588526a57c12a3d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-18 13:15:03 +0530

    Fixed bug and env file added

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const params = {
     headers:{
-        Authorization:"bearer"+ process.env.REACT_APP_STRIPE_APP },
+        Authorization:"bearer"+ process.env.REACT_APP_STRIPE_APP_KEY},
 };
 
 export const fetchDataFromApi = async (url) =>{
@@ -18,5 +18,5 @@ export const fetchDataFromApi = async (url) =>{
 export const makePaymentRequest = axios.create({
     baseURL: process.env.REACT_APP_DEV_URL,
     headers:{
-        Authorization:"bearer"+ process.env.REACT_APP_STRIPE_APP },
+        Authorization:"bearer"+ process.env.REACT_APP_STRIPE_APP_KEY },
 })
